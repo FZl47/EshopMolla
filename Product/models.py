21361fda37b165e9eab7c6bcb68aa2e2f6df02ac
FZl47 <fzl8747@gmail.com>
2022-02-11 04:38:22 +0330

    Add model Color and edit some field in models

--- Product/models.py
+++ Product/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from colorfield.fields import ColorField
 from User.models import User
 from Config.Tools import RandomString
 from django.shortcuts import resolve_url
-# Create your models here.
+from django.utils import timezone
+import datetime
 
+
+# Create your models here.
 
 
 class Category(models.Model):
@@ -11,36 +15,76 @@ class Category(models.Model):
 
     @property
     def getTitle(self):
-        return self.title.replace(' ','-')
+        return self.title.replace(' ', '-')
+
+    def get_absolute_url(self):
+        return f"{resolve_url('product:products')}?cat={self.getTitle}-{self.id}"
 
     def __str__(self):
         return self.title
 
 
+class ProductManagerCustomize(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status_show='active')
+
+
 class Product(models.Model):
     STATUS_SHOW = (
-        ('active','Active'),
-        ('deactive','Deactive'),
+        ('active', 'Active'),
+        ('deactive', 'Deactive'),
+    )
+    STATUS_AVAILABLE = (
+        ('available', 'Available'),
+        ('unavailable', 'Unavailable'),
     )
 
-    product_id = models.CharField(max_length=20,editable=False)
+    product_id = models.CharField(max_length=20, editable=False)
     title = models.CharField(max_length=300)
     description = models.TextField()
-    price = models.DecimalField(max_digits=12,decimal_places=2)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
     datecreate = models.DateTimeField(auto_now_add=True)
     dateupdate = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_SHOW,default='deactive')
+    status_show = models.CharField(max_length=10, choices=STATUS_SHOW, default='deactive')
+    status_available = models.CharField(max_length=20, choices=STATUS_AVAILABLE, default='available')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product')
-    images = models.ManyToManyField(to='Product.Image',related_name='product')
-    categories = models.ManyToManyField(Category,related_name='product')
+    images = models.ManyToManyField(to='Product.Image', related_name='product')
+    categories = models.ManyToManyField(Category, related_name='product')
+    colors = models.ManyToManyField('Product.Color', related_name='product')
+
+    objects = models.Manager()
+    getProducts = ProductManagerCustomize()
 
     class Meta:
         ordering = ('-id',)
 
+    @property
+    def gerPrice(self):
+        return self.price
 
     @property
     def get_absolue_url(self):
-        return f"{resolve_url('product:product')}/{self.title.replace(' ','-')}-{self.product_id}"
+        return f"{resolve_url('product:product')}/{self.title.replace(' ', '-')}-{self.product_id}"
+
+    @property
+    def getImageUrl(self):
+        images = self.images
+        if images.exists():
+            return images.first().image.url
+        # Image default for product
+        return '/assets/images/default/product/Default.jpg'
+
+    @property
+    def isNew(self):
+        # How long product is new
+        _day = 4
+        return True if (self.datecreate + datetime.timedelta(days=_day)) > timezone.now() else False
+
+    def getRating(self):
+        _rating = 2
+        #  5 * 20 => 100%
+        #  3 * 20 => 60%
+        return _rating * 20   # برای گرفتن ریتینگ به صورت درصد
 
     def __str__(self):
         return self.title
@@ -51,14 +95,26 @@ class Product(models.Model):
         super(Product, self).save(*args, **kwargs)
 
 
-
-def upload_src_image(instance,path):
+def upload_src_image(instance, path):
     _format = str(path).split('.')[-1]
     baseURL = 'products/images'
     return f'{baseURL}/{RandomString(20)}.{_format}'
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to=upload_src_image)
 
     def __str__(self):
         return 'image product'
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=30)
+    color = ColorField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def getName(self):
+        return self.name.replace(' ', '-')
