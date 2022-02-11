05e120c39d0c7819ca59b83a31c478ccffc75904
FZl47 <fzl8747@gmail.com>
2022-02-11 04:39:25 +0330

    Add one test for test get product active

--- Product/tests.py
+++ Product/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+class TestProductApp(TestCase):
+
+    def test_get_product_active(self):
+        products = models.Product.objects.exists()
+        if products:
+            prd = products.first()
+            self.assertEqual(prd.status_available == 'available')
+
+
