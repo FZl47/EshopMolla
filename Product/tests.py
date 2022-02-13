d0123ca46ef53535ada2cc298de3573a9ac0a6da
FZl47 <fzl8747@gmail.com>
2022-02-13 07:18:11 +0330

    add test page products

--- Product/tests.py
+++ Product/tests.py
@@ -10,3 +10,7 @@ class TestProductApp(TestCase):
             self.assertEqual(prd.status_available == 'available')
 
 
+    def test_page_products(self):
+        x = self.client.get('http://127.0.0.1:8000/p/products?filter=true&cats=[%22Shoes-1%22,%22T-shirt-3%22]&brands=[%22Nike-1%22,%22Addidass-2%22,%22Poma-3%22]&colors=[%22Blue-sky-1%22,%22red-2%22,%22orange-3%22,%22Black-4%22,%22Gray-5%22,%22Gold-6%22,%22White-7%22,%22Green-8%22,%22Green-Light-9%22,%22Blue-10%22,%22Blue-Light-11%22]&range=0-20000')
+        if x.status_code != 200:
+            raise Exception('Error in test page products')
