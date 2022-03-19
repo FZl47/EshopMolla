159c2d1e36bd1f6bc74f251e02ecc1142e31fdad
FZl47 <fzl8747@gmail.com>
2022-03-19 16:39:28 +0330

    Fix bug slug product

--- Product/urls.py
+++ Product/urls.py
@@ -6,12 +6,12 @@ app_name = 'Product'
 urlpatterns = [
     path('products',views.products.as_view(),name='products'),
     path('category/<str:title>',views.category.as_view(),name='category'),
-    path('product/<slug:slug>',views.product.as_view(),name='product'),
-    path('product/<slug:slug>/getStockProduct/<int:id>/<str:type>',views.getStockProduct,name='getStockProduct'),
-    path('product/<slug:slug>/addProductToCart',views.addProductToCart,name='addProductToCart'),
-    path('product/<slug:slug>/addProductToWishList',views.addProductToWishList,name='addProductToWishList'),
+    path('product/<slug>',views.product.as_view(),name='product'),
+    path('product/<slug>/getStockProduct/<int:id>/<str:type>',views.getStockProduct,name='getStockProduct'),
+    path('product/<slug>/addProductToCart',views.addProductToCart,name='addProductToCart'),
+    path('product/<slug>/addProductToWishList',views.addProductToWishList,name='addProductToWishList'),
     path('removeDetailCart',views.removeDetailCart,name='removeDetailCart'),
     path('removeDetailWish',views.removeDetailWish,name='removeDetailWish'),
-    path('product/<slug:slug>/addComment', views.addComment, name='addComment'),
+    path('product/<slug>/addComment', views.addComment, name='addComment'),
     path('checkCoupon', views.checkCoupon, name='checkCoupon'),
 ]
