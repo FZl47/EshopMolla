83fe4e76ee874e30e8ad6a4800b5305116de0588
Fazel Momeni <wwwfazel88@gmail.com>
2023-02-20 19:09:59 +0330

    Update urls.py

--- Config/urls.py
+++ Config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from pip._vendor.html5lib.treeadapters.sax import namespace
 from django.conf import settings
 from django.conf.urls.static import static
 
